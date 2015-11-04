a685b10de1454111f1337457af9e9b5ebdc35c44
Mark <mark@bhalash.com>
2015-11-04 16:41:36 +0000

    Streamlined problem #3 solution

--- solutions/ruby/03.rb
+++ solutions/ruby/03.rb
@@ -1,2 +1,7 @@
 require 'prime'
-puts 600851475143.prime_division.max[0]
+
+prime_factor = 600851475143.prime_division.map { |factor|
+    factor[0] if Prime.prime?(factor[0])
+}
+
+p prime_factor.max
