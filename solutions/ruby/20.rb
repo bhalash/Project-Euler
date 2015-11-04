a31c73d9274d0602a25c58ef9eebc6e7af43da25
Mark <mark@bhalash.com>
2015-11-04 18:33:01 +0000

    Streamlined solution #20

--- solutions/ruby/20.rb
+++ solutions/ruby/20.rb
@@ -2,6 +2,4 @@ def factorial(n)
     n > 1 ? n * factorial(n - 1) : 1
 end
 
-p factorial(100).to_s.split(//).map(&:to_i).inject { |sum, x|
-    sum + x
-}
+p factorial(100).to_s.split(//).map(&:to_i).reduce(:+)
